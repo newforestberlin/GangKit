0444a5456dc78936cc074522b15e5271506fa9d1
Ricki Gregersen <hi@rickigregersen.com>
2015-11-09 17:11:09 +0100

    Initial Commit

---
+++ GangKit/GangKit.h
@@ -0,0 +1,19 @@
+//
+//  GangKit.h
+//  GangKit
+//
+//  Created by Ricki Gregersen on 09/11/15.
+//  Copyright © 2015 youandthegang.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GangKit.
+FOUNDATION_EXPORT double GangKitVersionNumber;
+
+//! Project version string for GangKit.
+FOUNDATION_EXPORT const unsigned char GangKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GangKit/PublicHeader.h>
+
+
