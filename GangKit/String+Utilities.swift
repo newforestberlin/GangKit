45cdc018c813d3956f6e9407990d2f4bd49cabd9
Ricki Gregersen <hi@rickigregersen.com>
2016-01-12 12:02:26 +0100

    made string utilities public

--- GangKit/String+Utilities.swift
+++ GangKit/String+Utilities.swift
@@ -24,7 +24,7 @@
 
 import Foundation
 
-extension String {
+public extension String {
     
     //MARK: Helper methods
     
@@ -34,11 +34,11 @@ extension String {
     - returns: Int length of the string.
     */
     
-    var length: Int {
+    public var length: Int {
         return self.characters.count
     }
     
-    var objcLength: Int {
+    public var objcLength: Int {
         return self.utf16.count
     }
     
@@ -51,7 +51,7 @@ extension String {
     
     - returns: String! Returns a string representing the langague of the string (e.g. en, fr, or und for undefined).
     */
-    func detectLanguage() -> String? {
+    public func detectLanguage() -> String? {
         if self.length > 4 {
             let tagger = NSLinguisticTagger(tagSchemes:[NSLinguisticTagSchemeLanguage], options: 0)
             tagger.string = self
@@ -65,7 +65,7 @@ extension String {
      
      - returns: String! returns a string representing the script of the String (e.g. Latn, Hans).
      */
-    func detectScript() -> String? {
+    public func detectScript() -> String? {
         if self.length > 1 {
             let tagger = NSLinguisticTagger(tagSchemes:[NSLinguisticTagSchemeScript], options: 0)
             tagger.string = self
@@ -82,7 +82,7 @@ extension String {
      - returns: Bool The Bool will return true if the string was writting in a right to left langague (e.g. Arabic, Hebrew)
      
      */
-    var isRightToLeft : Bool {
+    public var isRightToLeft : Bool {
         let language = self.detectLanguage()
         return (language == "ar" || language == "he")
     }
@@ -95,7 +95,7 @@ extension String {
     
     - returns: Bool
     */
-    func isOnlyEmptySpacesAndNewLineCharacters() -> Bool {
+    public func isOnlyEmptySpacesAndNewLineCharacters() -> Bool {
         return self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()).length == 0
     }
     
@@ -104,7 +104,7 @@ extension String {
      
      - returns: Bool
      */
-    var isEmail: Bool {
+    public var isEmail: Bool {
         let dataDetector = try? NSDataDetector(types: NSTextCheckingType.Link.rawValue)
         let firstMatch = dataDetector?.firstMatchInString(self, options: NSMatchingOptions.ReportCompletion, range: NSMakeRange(0, length))
         
@@ -116,7 +116,7 @@ extension String {
      
      - returns: Bool
      */
-    func isTweetable() -> Bool {
+    public func isTweetable() -> Bool {
         let tweetLength = 140,
         // Each link takes 23 characters in a tweet (assuming all links are https).
         linksLength = self.getLinks().count * 23,
@@ -134,7 +134,7 @@ extension String {
      
      - returns: [String]
      */
-    func getLinks() -> [String] {
+    public func getLinks() -> [String] {
         let detector = try? NSDataDetector(types: NSTextCheckingType.Link.rawValue)
         
         let links = detector?.matchesInString(self, options: NSMatchingOptions.ReportCompletion, range: NSMakeRange(0, length)).map {$0 }
@@ -151,7 +151,7 @@ extension String {
      
      - returns: [NSURL]
      */
-    func getURLs() -> [NSURL] {
+    public func getURLs() -> [NSURL] {
         let detector = try? NSDataDetector(types: NSTextCheckingType.Link.rawValue)
         
         let links = detector?.matchesInString(self, options: NSMatchingOptions.ReportCompletion, range: NSMakeRange(0, length)).map {$0 }
@@ -169,7 +169,7 @@ extension String {
      
      - returns: [NSDate]
      */
-    func getDates() -> [NSDate] {
+    public func getDates() -> [NSDate] {
         let error: NSErrorPointer = NSErrorPointer()
         let detector: NSDataDetector?
         do {
@@ -192,7 +192,7 @@ extension String {
      
      - returns: [String]
      */
-    func getHashtags() -> [String]? {
+    public func getHashtags() -> [String]? {
         let hashtagDetector = try? NSRegularExpression(pattern: "#(\\w+)", options: NSRegularExpressionOptions.CaseInsensitive)
         let results = hashtagDetector?.matchesInString(self, options: NSMatchingOptions.WithoutAnchoringBounds, range: NSMakeRange(0, self.utf16.count)).map { $0 }
         
@@ -206,7 +206,7 @@ extension String {
      
      - returns: [String]
      */
-    func getUniqueHashtags() -> [String]? {
+    public func getUniqueHashtags() -> [String]? {
         return Array(Set(getHashtags()!))
     }
     
@@ -217,7 +217,7 @@ extension String {
      
      - returns: [String]
      */
-    func getMentions() -> [String]? {
+    public func getMentions() -> [String]? {
         let hashtagDetector = try? NSRegularExpression(pattern: "@(\\w+)", options: NSRegularExpressionOptions.CaseInsensitive)
         let results = hashtagDetector?.matchesInString(self, options: NSMatchingOptions.WithoutAnchoringBounds, range: NSMakeRange(0, self.utf16.count)).map { $0 }
         
@@ -231,7 +231,7 @@ extension String {
      
      - returns: Bool with true value if it does
      */
-    func getUniqueMentions() -> [String]? {
+    public func getUniqueMentions() -> [String]? {
         return Array(Set(getMentions()!))
     }
     
@@ -241,7 +241,7 @@ extension String {
      
      - returns: Bool with true value if it does
      */
-    func containsLink() -> Bool {
+    public func containsLink() -> Bool {
         return self.getLinks().count > 0
     }
     
@@ -250,14 +250,14 @@ extension String {
      
      - returns: Bool with true value if it does
      */
-    func containsDate() -> Bool {
+    public func containsDate() -> Bool {
         return self.getDates().count > 0
     }
     
     /**
      - returns: Base64 encoded string
      */
-    func encodeToBase64Encoding() -> String {
+    public func encodeToBase64Encoding() -> String {
         let utf8str = self.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!
         return utf8str.base64EncodedStringWithOptions(NSDataBase64EncodingOptions.Encoding64CharacterLineLength)
     }
@@ -265,7 +265,7 @@ extension String {
     /**
      - returns: Decoded Base64 string
      */
-    func decodeFromBase64Encoding() -> String {
+    public func decodeFromBase64Encoding() -> String {
         let base64data = NSData(base64EncodedString: self, options: NSDataBase64DecodingOptions.IgnoreUnknownCharacters)
         return NSString(data: base64data!, encoding: NSUTF8StringEncoding)! as String
     }
@@ -274,23 +274,23 @@ extension String {
     
     // MARK: Subscript Methods
     
-    subscript (i: Int) -> String {
+    public subscript (i: Int) -> String {
         return String(Array(self.characters)[i])
     }
     
-    subscript (r: Range<Int>) -> String {
-        var start = startIndex.advancedBy(r.startIndex),
-        end = startIndex.advancedBy(r.endIndex)
+    public subscript (r: Range<Int>) -> String {
+        let start = startIndex.advancedBy(r.startIndex)
+        let end = startIndex.advancedBy(r.endIndex)
         
         return substringWithRange(Range(start: start, end: end))
     }
     
-    subscript (range: NSRange) -> String {
+    public subscript (range: NSRange) -> String {
         let end = range.location + range.length
         return self[Range(start: range.location, end: end)]
     }
     
-    subscript (substring: String) -> Range<String.Index>? {
+    public subscript (substring: String) -> Range<String.Index>? {
         return rangeOfString(substring, options: NSStringCompareOptions.LiteralSearch, range: Range(start: startIndex, end: endIndex), locale: NSLocale.currentLocale())
     }
 }
