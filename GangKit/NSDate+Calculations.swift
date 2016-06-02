ade63d29833bee3896d346d472799c419d1fefc6
Ricki Gregersen <hi@rickigregersen.com>
2016-06-02 10:28:51 +0200

    added Comparable conformance to NSDate

---
+++ GangKit/NSDate+Calculations.swift
@@ -0,0 +1,19 @@
+//
+//  NSDate+Calculations.swift
+//  GangKit
+//
+//  Created by Ricki Gregersen on 02/06/16.
+//  Copyright © 2016 youandthegang.com. All rights reserved.
+//
+
+import Foundation
+
+public func <(a: NSDate, b: NSDate) -> Bool {
+	return a.compare(b) == NSComparisonResult.OrderedAscending
+}
+
+public func ==(a: NSDate, b: NSDate) -> Bool {
+	return a.compare(b) == NSComparisonResult.OrderedSame
+}
+
+extension NSDate: Comparable { }
